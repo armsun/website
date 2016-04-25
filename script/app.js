e6b40d3335e3cd0a74089f8cebe95485a4e2f4d1
Arman <armangevorgyan@yahoo.com>
2016-04-26 03:55:13 +0400

    carusel

--- script/app.js
+++ script/app.js
@@ -6,7 +6,18 @@ $(function() {
   .mouseleave(function(){
     $(this).removeClass("animated pulse")
   });
-
+  $('.carusel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow:1,
+   /* adaptiveHeight: true,*/
+     fade: true,
+     cssEase: 'linear',
+     
+     autoplay: true,
+     autoplaySpeed: 2000
+  });
     
   
 });
