29cfe3547d24ccf6cb7fd7563d997619ab11f96c
Arman <armangevorgyan@yahoo.com>
2016-04-27 01:27:05 +0400

    pre-final polish

--- script/app.js
+++ script/app.js
@@ -18,6 +18,26 @@ $(function() {
      autoplay: true,
      autoplaySpeed: 2000
   });
+    /*maps*/
+    var myCenter=new google.maps.LatLng(40.150830, 44.031469);
     
+    function initialize()
+    {
+    var mapProp = {
+      center:myCenter,
+      zoom:15,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+
+    var map=new google.maps.Map(document.getElementById("Map"),mapProp);
+
+    var marker=new google.maps.Marker({
+      position:myCenter,
+      });
+
+    marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
   
 });
