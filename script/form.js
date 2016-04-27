b87ae69126c8cc7a340719bcc60a800a614199c7
Arman <armangevorgyan@yahoo.com>
2016-04-27 16:44:02 +0400

    final

--- script/form.js
+++ script/form.js
@@ -1,13 +1,14 @@
 /*popup form send message*/
-  // Validating Empty Field
-  function check_empty() {
+  // Validation
+
+   function check_empty() {
     if (document.getElementById('cname').value == "" 
     || document.getElementById('cemail').value == "" 
     || document.getElementById('cmsg').value == "") {
       alert("Fill All Fields !");
     } else {
       document.getElementById('form').submit();
-      alert("Form Submitted Successfully...");
+      
     }
   }
   //Function To Display Popup
@@ -18,3 +19,4 @@
   function div_hide(){
     document.getElementById('popupform').style.display = 'none';
   }
+ 
